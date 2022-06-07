5dabf0466850e0cbb4a1d6d64370e4a1c8bb3ebe
Edouard Misset <edouard.misset@intest.info>
2022-06-07 10:31:40 +0200

    [useFetch] Added a loading indicator to the hook

--- src/useFetch.tsx
+++ src/useFetch.tsx
@@ -5,22 +5,26 @@ const useFetch: (
   options: RequestInit,
 ) => { response: any; error: Error | unknown } = (url, options) => {
   const [response, setResponse] = useState<unknown>(null)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
   const [error, setError] = useState<Error | unknown>(null)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setIsLoading(true)
         const res = await fetch(url, options)
         const json = await res.json()
         setResponse(json)
       } catch (error) {
         setError(error)
+      } finally {
+        setIsLoading(false)
       }
     }
     fetchData()
   }, [options, url])
 
-  return { response, error }
+  return { response, error, isLoading }
 }
 
 export default useFetch
