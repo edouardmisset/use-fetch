e65c6b1a23e8e36b2170fc936e137d98a0a8b6ce
Edouard Misset <edouard.misset@intest.info>
2022-09-27 14:07:12 +0200

    Added the `useMediaQuery` hook

---
+++ src/useMediaQuery.tsx
@@ -0,0 +1,38 @@
+import { useCallback, useEffect, useState } from 'react'
+
+export const useMediaQuery = (query: string): boolean => {
+  const getMatches = useCallback(
+    (query: string): boolean =>
+      // Prevents SSR issues
+      typeof window !== 'undefined' ? window.matchMedia(query).matches : false,
+    [],
+  )
+
+  const [matches, setMatches] = useState<boolean>(getMatches(query))
+
+  const handleChange = useCallback(() => setMatches(getMatches(query)), [query])
+
+  useEffect(() => {
+    const matchMedia = window.matchMedia(query)
+
+    // Triggered at the first client-side load and if query changes
+    handleChange()
+
+    // Listen matchMedia
+    if (matchMedia.addListener) {
+      matchMedia.addListener(handleChange)
+    } else {
+      matchMedia.addEventListener('change', handleChange)
+    }
+
+    return () => {
+      if (matchMedia.removeListener) {
+        matchMedia.removeListener(handleChange)
+      } else {
+        matchMedia.removeEventListener('change', handleChange)
+      }
+    }
+  }, [handleChange, query])
+
+  return matches
+}
