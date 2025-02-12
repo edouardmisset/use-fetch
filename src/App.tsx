35f52fc87ee1017b455749c7e0aba2b6f0da2b1b
Edouard Misset <edouardmisset@gmail.com>
2025-02-12 19:10:49 +0100

    fix: fix useFetch abort controller

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import useFetch from './useFetch'
 
 const ImageFetch = (): JSX.Element => {
   const { response, error } = useFetch({
-    url: 'https://dog.ceo/api/breeds/image/random',
+    url: 'https://dog.ceo/api/breed/samoyed/images/random',
   })
   if (!response) return <div>Loading...</div>
   if (error) return <div>Something wrong happened 😒</div>
--- src/useFetch.ts
+++ src/useFetch.ts
@@ -1,29 +1,31 @@
 import { useEffect, useState } from 'react'
 
-const useFetch = ({
-  url,
-  options,
-}: {
-  url: string
-  options?: ResponseInit
-}): {
+type UseFetchResult = {
   response: any
   error: Error | unknown
   isLoading: boolean
   abort: VoidFunction
-} => {
+}
+
+type UseFetchProps = {
+  url: string
+  options?: ResponseInit
+}
+
+const useFetch = (props: UseFetchProps): UseFetchResult => {
+  const { url, options } = props
   const [response, setResponse] = useState<unknown>(null)
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [error, setError] = useState<Error | unknown>(null)
-  const [abort, setAbort] = useState<VoidFunction>(() => {})
+  const [abort, setAbort] = useState<(reason?: any) => void>(() => { })
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         setIsLoading(true)
-        const abortController = new AbortController()
-        const { signal } = abortController
-        setAbort(abortController.abort)
+        const controller = new AbortController()
+        const { signal } = controller
+        setAbort(() => controller.abort)
         const res = await fetch(url, { ...options, signal })
         const json = await res.json()
         setResponse(json)
