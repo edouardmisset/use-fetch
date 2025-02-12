6aa3082d18d502e36413655ce8fe11bb6402ebdb
Edouard Misset <edouardmisset@gmail.com>
2025-02-12 19:29:09 +0100

    fix(useToggle): simplify default value handling and variable naming

--- src/useToggle.ts
+++ src/useToggle.ts
@@ -1,10 +1,8 @@
 import { useState, useCallback, Dispatch, SetStateAction } from 'react'
 
-export const useToggle = (
-  defaultValue?: boolean,
-): [boolean, Dispatch<SetStateAction<boolean>>] => {
-  const [value, setValue] = useState<boolean>(Boolean(defaultValue))
+export const useToggle = (defaultValue = false): [boolean, Dispatch<SetStateAction<boolean>>] => {
+  const [bool, setBool] = useState(defaultValue)
   // Define and memorize toggler function in case we pass down the component,
-  const toggle = useCallback(() => setValue(val => !val), [])
-  return [value, toggle]
+  const toggle = useCallback(() => setBool(b => !b), [])
+  return [bool, toggle]
 }
